7737b78cd97a081b88c29465f7856bfa6de24c9f
edethy <edethy@mit.edu>
2014-11-29 21:47:11 -0500

    updates

--- aoa_array_all_signals.m
+++ aoa_array_all_signals.m
@@ -1,24 +1,22 @@
-function [ theta_array ] = aoa_array_all_signals( csi_all_signals, d )
+function [ theta_array ] = aoa_array_all_signals( csi_all_signals, d, lamda )
 %UNTITLED24 Summary of this function goes here
 %   Detailed explanation goes here
 
-    freq=5825*10^6; %%
-    c=3*10^8;
-    lamda=c/freq;
+    
     
     csi_size = size(csi_all_signals);
     
-    phase_1_array = NaN(csi_size(4), 1);
-    phase_2_array = NaN(csi_size(4), 1);
-    phase_3_array = NaN(csi_size(4), 1);
+    %phase_1_array = NaN(csi_size(4), 1);
+    %phase_2_array = NaN(csi_size(4), 1);
+    %phase_3_array = NaN(csi_size(4), 1);
     
     theta_array = NaN(csi_size(4), 2);
     for i=1:1:csi_size(4)
         csi_data = csi_all_signals(:,:,:,i);
-        phases = avg_phase(csi_data);
-        phase_1_array(i) = phases(1);
-        phase_2_array(i) = phases(2);
-        phase_3_array(i) = phases(3);
-        theta_array(i,:) = get_aoa(phases(1), phases(2), phases(3), d, lamda);
+        theta_array(i,:) = avg_angle(csi_data, d, lamda);
+        %phase_1_array(i) = phases(1);
+        %phase_2_array(i) = phases(2);
+        %phase_3_array(i) = phases(3);
+        %theta_array(i,:) = get_aoa(phases(1), phases(2), phases(3), d, lamda);
     end
 
--- plot_experiments_smooth.m
+++ plot_experiments_smooth.m
@@ -1,14 +1,32 @@
-log_data = read_bf_file_transmitter('csi-00216a4838ca-experiment1.dat');
+clear;
 
-sender_1 = [0;33;106;107;54;48];
-sender_2 = [0;33;106;2;85;14];
+%%% Start constants
+log_data_wagon = read_bf_file_transmitter('csi-00216a02550e-experiment1.dat');
+log_data_right_stool = read_bf_file_transmitter('csi-00216a6b3630-experiment1.dat');
+log_data_left_stool = read_bf_file_transmitter('csi-00216a4838ca-experiment1.dat');
 
-sender_1_csi = separate_senders(log_data, sender_1);
-sender_2_csi = separate_senders(log_data, sender_2);
+left_stool_dec = [0;33;106;72;56;202];
+right_stool_dec = [0;33;106;107;54;48];
+wagon_dec = [0;33;106;2;85;14];
 
-sender1_aoa_arrays = aoa_array_all_signals(sender_1_csi, .019);
-sender2_aoa_arrays = aoa_array_all_signals(sender_2_csi, .019);
+d = .025;
+freq=5825*10^6;
+c=3*10^8;
+lamda=c/freq;
 
+sender_1 = wagon_dec;
+sender_2 = left_stool_dec;
+%%% End Constants
+
+%%% Differentiate senders in log data
+sender_1_csi = separate_senders(log_data_right_stool, sender_1);
+sender_2_csi = separate_senders(log_data_right_stool, sender_2);
+
+%%% Get average angle of arrival for different senders
+sender1_aoa_arrays = aoa_array_all_signals(sender_1_csi, d, lamda);
+sender2_aoa_arrays = aoa_array_all_signals(sender_2_csi, d, lamda);
+
+%%% Isolate different aoa's from 
 s1_aoa21 = sender1_aoa_arrays(:,1);
 s1_aoa32 = sender1_aoa_arrays(:,2);
 
@@ -24,10 +42,12 @@ s2_aoa32_smooth = smooth(s2_aoa32, 'rlowess');
 x1 = 1:1:length(s1_aoa21');
 x2 = 1:1:length(s2_aoa32');
 figure(1);
+clf('reset');
 hold on;
 plot(x1, s1_aoa21_smooth, 'red');
 plot(x1, s1_aoa32_smooth, 'blue');
 figure(2);
+clf('reset');
 hold on;
 plot(x2, s2_aoa21_smooth, 'red');
 plot(x2, s2_aoa32_smooth, 'blue');
